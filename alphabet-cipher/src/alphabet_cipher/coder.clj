b428d81a2a44e444630745801a4d6f899be890cb
HongJoon Ahn <anvatar@gmail.com>
2021-04-28 16:17:57 +0900

    Alphabet cipher (Week 4, Day 1—2) (#1)
    * Implement `encode`
    
    Week 4, Day 1
    
    * Simplify `encode-rows` and `encode`
    
    * Reformat `encode-table`
    
    * Implement `decode`
    
    Week 4, Day 2
    
    * Implement `decipher`

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,43 @@
 (ns alphabet-cipher.coder)
 
+(def alphabet "abcdefghijklmnopqrstuvwxyz")
+
+(def encode-rows
+  (map seq
+       (iterate #(str (subs % 1) (subs % 0 1))
+                alphabet)))
+
+(def encode-table
+  (into {}
+        (for [[r row] (zipmap alphabet encode-rows)
+              [c letter] (zipmap alphabet row)]
+             [[r c] letter])))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str
+         (map (fn [r c] (get encode-table [r c]))
+              (apply concat (repeat keyword))
+              message)))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str
+         (map #(-> (filter (fn [[[r _] x]] (and (= r %1) (= x %2))) encode-table)
+                   first
+                   first
+                   second)
+              (apply concat (repeat keyword))
+              message)))
+
+(defn shortest-prefix [stretched]
+  (last (filter #(= (take (count stretched) (apply concat (repeat %))) stretched)
+                (take-while not-empty (iterate #(drop-last %) stretched)))))
 
 (defn decipher [cipher message]
-  "decypherme")
-
+  (apply str
+         (shortest-prefix
+           (map #(-> (filter (fn [[[_ c] x]] (and (= c %1) (= x %2))) encode-table)
+                     first
+                     first
+                     first)
+                message
+                cipher))))
