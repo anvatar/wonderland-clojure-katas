e1a4b51b80be3370d34257db520823532cc721e7
HongJoon Ahn <holden.ahn@linecorp.com>
2021-05-17 17:36:52 +0900

    Move some query functions from `puzzle_test.clj` to `puzzle.clj`
    I'm going to make use of these functions in `magic-square`

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -2,6 +2,18 @@
 
 (def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
+(defn sum-rows [m]
+  (map #(reduce + %) m))
+
+(defn sum-cols [m]
+  [(reduce + (map first m))
+   (reduce + (map second m))
+   (reduce + (map last m))])
+
+(defn sum-diagonals [m]
+  [(+ (get-in m [0 0]) (get-in m [1 1]) (get-in m [2 2]))
+   (+ (get-in m [2 0]) (get-in m [1 1]) (get-in m [0 2]))])
+
 (defn magic-square [values]
   [[1.0 1.5 2.0]
    [2.5 3.0 3.5]
