d2f6f82d907ef5f2b5b1dded60fdaa850c4ae918
HongJoon Ahn <anvatar@gmail.com>
2021-05-21 20:09:46 +0900

    Tiny maze (Week 6, Day 1–2) (#6)

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,29 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+(defn- escaped? [maze]
+  (= :x
+     (last (last maze))))
+
+(defn- mark [maze [r c]]
+  (assoc maze r
+              (assoc (nth maze r) c
+                                  :x)))
+
+(defn- next-positions [maze [r c]]
+  (for [nr (range (count maze))
+        nc (range (count (first maze)))
+        :when (and (some? (#{0 :E} (get-in maze [nr nc])))
+                   (= [0 1]
+                      (sort (map #(Math/abs ^int %)
+                                 [(- nr r) (- nc c)]))))]
+    [nr nc]))
+
+(defn solve-maze [maze]
+  (loop [queue [[(mark maze [0 0]) [0 0]]]]
+    (let [[mz pos] (first queue)]
+      (if (escaped? mz)
+        mz
+        (recur (apply conj
+                      (rest queue)
+                      (map #(vector (mark mz %) %)
+                           (next-positions mz pos))))))))
