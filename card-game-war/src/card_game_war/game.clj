4a6d5e5dd7dc5d6a2ee0c53c08a3667f30cc7102
HongJoon Ahn <holden.ahn@linecorp.com>
2021-06-10 11:12:35 +0900

    Implement `play-game`

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,33 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn play-round [[_ rank0] [_ rank1]]
+  (let [indexes (zipmap ranks (range))
+        [index0 index1] (map #(get indexes %) [rank0 rank1])]
+    (cond (> index0 index1) 0
+          (< index0 index1) 1
+          :else nil)))
 
-(defn play-game [player1-cards player2-cards])
+(defn- show-cards [cards]
+  (println (take 5 cards) " (total " (count cards) " cards)"))
+
+(defn play-game [player0-cards player1-cards]
+  (loop [cards0 player0-cards
+         cards1 player1-cards
+         war-cards []]
+    (println "-----")
+    (doall (map show-cards [cards0 cards1 war-cards]))
+
+    (cond (every? empty? [cards0 cards1]) nil
+          (empty? cards1) 0
+          (empty? cards0) 1
+          :else (case (play-round (first cards0) (first cards1))
+                  0 (recur (concat (rest cards0) (shuffle (concat war-cards (map first [cards0 cards1]))))
+                           (rest cards1)
+                           [])
+                  1 (recur (rest cards0)
+                           (concat (rest cards1) (shuffle (concat war-cards (map first [cards0 cards1]))))
+                           [])
+                  nil (recur (drop 4 cards0)
+                             (drop 4 cards1)
+                             (concat war-cards (take 4 cards0) (take 4 cards1)))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -6,11 +6,41 @@
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings"))
+    (is (= 0 (play-round [:spade 10] [:club 2])))
+    (is (= 1 (play-round [:club 2] [:diamond 10])))
+    (is (= nil (play-round [:diamond 5] [:heart 5]))))
+  (testing "queens are higher rank than jacks"
+    (is (= 0 (play-round [:spade :queen] [:club :jack]))))
+  (testing "kings are higher rank than queens"
+    (is (= 0 (play-round [:spade :king] [:club :queen]))))
+  (testing "aces are higher rank than kings"
+    (is (= 0 (play-round [:spade :ace] [:club :king])))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (contains? [0 1 nil]
+               (play-game [[:spade 4]
+                           [:spade :ace]
+                           [:spade 6]
+                           [:club 4]
+                           [:club :ace]
+                           [:club 6]
+                           [:diamond 8]]
+                          [[:spade 8]
+                           [:spade :king]
+                           [:diamond 6]
+                           [:heart 8]
+                           [:heart :king]
+                           [:heart 6]
+                           [:heart 9]]))))
 
+(deftest random-play-game
+  (dotimes [n 10]
+    (println "=====")
+    (println "Random game No." n)
+    (testing
+      (let [shuffled-pairs (partition 2 (shuffle cards))
+            player0-cards (map first shuffled-pairs)
+            player1-cards (map second shuffled-pairs)]
+        (contains? [0 1 nil]
+                   (play-game player0-cards player1-cards))))))
